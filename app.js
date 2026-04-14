4673c9ce96d7be5f121ea1a26772f0858be5932d
dalvinikita21-gif <dalvinikita21@gmail.com>
2026-04-14 12:56:33 +0530

    Billing logic added

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Retail Billing System Running");
+
+function generateBill() {
+    let price = document.getElementById("price").value;
+    let quantity = document.getElementById("quantity").value;
+
+    let total = price * quantity;
+    let gst = total * 0.18;
+    let finalAmount = total + gst;
+
+    document.getElementById("result").innerHTML =
+        "Total: ₹" + total +
+        "<br>GST (18%): ₹" + gst +
+        "<br>Final Amount: ₹" + finalAmount;
+}
